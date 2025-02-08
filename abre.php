26070833c1c954a96539be0fd734d4fb761d922a
Edd <eddnomas@gmail.com>
2025-02-08 11:25:00 -0700

    Add files via upload

---
+++ abre.php
@@ -0,0 +1,11 @@
+<?php
+    $conexion = new mysqli("localhost","root","","cines");
+    if($conexion){
+        echo "la gestion fue exitosa !!!";
+    }else{
+        echo "algo salio mal";
+    }
+
+
+
+?>
